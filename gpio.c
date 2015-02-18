e6134d44cd7dd02aa9c177f73d8f6b0c1fa4987e
Matt Schulte <schultetwin1@gmail.com>
2015-02-17 23:23:12 -0500

    Added inline function

--- gpio.c
+++ gpio.c
@@ -3,6 +3,10 @@
 #define GPIOA_BASEADDR 0x50000000
 #define GPIOX_BASEADDR(x) (GPIOA_BASEADDR + (0x0400 * x))
 
+static inline uint32_t* gpiox_baseaddr(uint8_t port) {
+  return (uint32_t*)(GPIOA_BASEADDR + (0x0400 * port));
+}
+
 void gpio_init(uint8_t port) {
   // Enable the GPIO timer
   *((unsigned *)(0X40021000 + 0x2C)) |= (1 << port);
@@ -12,25 +16,25 @@ void gpio_set_mode(uint8_t port, uint8_t pin_num, gpio_mode_t mode) {
   uint32_t modes;
   uint32_t types;
 
-  modes = *((uint32_t*)(GPIOX_BASEADDR(port)));
+  modes = *gpiox_baseaddr(port);
   modes &= ~(3 << (pin_num * 2));
   modes |= (mode << (pin_num * 2));
-  *((uint32_t*)(GPIOX_BASEADDR(port))) = modes;
+  *gpiox_baseaddr(port) = modes;
 
   if (mode == GPIO_OUTPUT_MODE) {
-    types = *((uint32_t*)(GPIOX_BASEADDR(port) + 4));
+    types = *(gpiox_baseaddr(port) + 1);
     types &= ~(1 << pin_num);
-    *((uint32_t*)(GPIOX_BASEADDR(port) + 4)) = types;
+    *(gpiox_baseaddr(port) + 1) = types;
   }
 }
 
 void gpio_write(uint8_t port, uint8_t pin_num, uint8_t write) {
   uint32_t data;
-  data = *((uint32_t*)(GPIOX_BASEADDR(port) + 0x18));
+  data = *(gpiox_baseaddr(port) + (0x18 / 4));
   if (write) {
-    *((uint32_t*)(GPIOX_BASEADDR(port) + 0x18)) = data | (1 << pin_num);
+    *(gpiox_baseaddr(port) + 0x18 / 4) = data | (1 << pin_num);
   } else {
-    *((uint32_t*)(GPIOX_BASEADDR(port) + 0x18)) = data | (1 << (pin_num + 16));
+    *(gpiox_baseaddr(port) + 0x18 / 4) = data | (1 << (pin_num + 16));
 
   }
 }
