489d35e05aae38ea5c0958287f00c67e5682dba0
Matt Schulte <schultetwin1@gmail.com>
2015-02-19 10:44:02 -0500

    Added two timers to run

--- blink.c
+++ blink.c
@@ -4,7 +4,13 @@
 #include "vector.h"
 #include "vtimer.h"
 
-void toggle_led() {
+void toggle_ledA() {
+  static int toggle = 0;
+  gpio_write(GPIOA, 5, toggle);
+  toggle ^= 1;
+}
+
+void toggle_ledB() {
   static int toggle = 0;
   gpio_write(GPIOB, 4, toggle);
   toggle ^= 1;
@@ -20,9 +26,12 @@ int main() {
   gpio_write(GPIOB, 4, 1);
   gpio_write(GPIOA, 5, 1);
 
-  vtimer_t toggle;
+  vtimer_t toggleA;
+  vtimer_t toggleB;
   vtimer_init();
-  vtimer_add_timer(&toggle, toggle_led, 100, 0);
+  vtimer_add_timer(&toggleB, toggle_ledB, 2000, 0);
+  vtimer_add_timer(&toggleA, toggle_ledA, 4000, 0);
+  vtimer_enable();
   while (1)
     ;
   return 0;
