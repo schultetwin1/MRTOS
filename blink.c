56d7d391a87f0585f1f90ab8a682254916d007f8
Matt Schulte <schultetwin1@gmail.com>
2015-02-20 10:08:26 -0500

    Fixed up for testing

--- blink.c
+++ blink.c
@@ -17,20 +17,18 @@ void toggle_ledB() {
 }
 
 int main() {
-
   gpio_init(GPIOA);
   gpio_init(GPIOB);
   
-  gpio_set_mode(GPIOB, 4, GPIO_OUTPUT_MODE);
   gpio_set_mode(GPIOA, 5, GPIO_OUTPUT_MODE);
-  gpio_write(GPIOB, 4, 1);
-  gpio_write(GPIOA, 5, 1);
+  gpio_set_mode(GPIOB, 4, GPIO_OUTPUT_MODE);
+
 
   vtimer_t toggleA;
   vtimer_t toggleB;
   vtimer_init();
-  vtimer_add_timer(&toggleB, toggle_ledB, 2000, 0);
-  vtimer_add_timer(&toggleA, toggle_ledA, 4000, 0);
+  vtimer_add_timer(&toggleA, toggle_ledA, 1, 0);
+  vtimer_add_timer(&toggleB, toggle_ledB, 2, 0);
   vtimer_enable();
   while (1)
     ;
