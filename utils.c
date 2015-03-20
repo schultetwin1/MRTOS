d03c02e324888df32a2623b4f5fc31c0038a95fc
Matt Schulte <schultetwin1@gmail.com>
2015-03-20 09:23:39 -0400

    Added simple malloc

--- utils.c
+++ utils.c
@@ -1,6 +1,5 @@
-#include <inttypes.h>
-
 #include "utils.h"
+extern int* _heap;
 
 // @TODO: Work for different clocks
 // Assuming a 2.097Mhz clock
@@ -9,4 +8,15 @@ void delay(uint16_t ms) {
   const int MAX = ms * 209;
   volatile int i;
   for (i = 0; i < MAX; i++);
+}
+
+// @TODO: Build better malloc and free implementations
+void* malloc(unsigned int bytes) {
+  void* space = _heap;
+  _heap = _heap + (bytes >> 2) + 1;
+  return space;
+}
+
+void free(void* ptr) {
+  // MT
 }
--- utils.h
+++ utils.h
@@ -1,4 +1,9 @@
 #ifndef __MRTOS_UTILS__
 #define __MRTOS_UTILS__
+#include <inttypes.h>
+#include <stddef.h>
+
 void delay(uint16_t ms);
+void* malloc(unsigned int bytes);
+void free(void* ptr);
 #endif
