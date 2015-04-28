1ad13419dbd4e571364aafac3a9103a49241827d
Matt Schulte <schultetwin1@gmail.com>
2015-04-28 08:21:07 -0400

    Updating epd but still not working

--- port/gcc/m0+/drivers/epd.c
+++ port/gcc/m0+/drivers/epd.c
@@ -14,6 +14,7 @@
 // spi set to 8 bit frame  (SPI1->CR1 &= ~(SPI_DFF))
 // spi enable nss          (SPI1->CR2 |= SPI_SSOE)
 
+#include "utils.h"
 #include "drivers/gpio.h"
 #include "drivers/spi.h"
 #include "drivers/port_epd.h"
@@ -26,6 +27,9 @@ void epd_init() {
   gpio_set_mode(GPIOA, 8, GPIO_INPUT_MODE);
   // ePD1_RESET = PB2
   gpio_set_mode(GPIOB, 2, GPIO_OUTPUT_MODE);
+  gpio_set_type(GPIOB, 2, GPIO_TYPE_PUSH_PULL);
+  gpio_set_pull(GPIOB, 2, GPIO_PULLUP);
+  gpio_write(GPIOB, 2, 1);
   // ePD1_D/C   = PB11
   gpio_set_mode(GPIOB, 11, GPIO_OUTPUT_MODE);
   // ePD1_PWR_ENn = PB10
@@ -46,8 +50,71 @@ void epd_init() {
   SPI1->CR1 &= ~(SPI_DFF);
   // spi enable nss
   SPI1->CR2 |= SPI_SSOE;
+
+  spi_enable();
 }
 
 void epd_pwr_en() {
   gpio_write(GPIOB, 10, 1);
+}
+
+void epd_reset() {
+  gpio_write(GPIOB, 2, 0);
+  delay(1);
+  gpio_write(GPIOB, 2, 1);
+}
+
+uint8_t epd_busy() {
+  return gpio_read(GPIOA, 8);
+}
+
+void epd_cmd(uint8_t cmd) {
+  while (epd_busy())
+    ;
+  gpio_write(GPIOB, 11, 0);
+  spi_write(cmd);
+}
+
+void epd_data(uint8_t data) {
+  while (epd_busy())
+    ;
+  gpio_write(GPIOB, 11, 1);
+  spi_write(data);
+}
+
+void epd_clr() {
+  // Deep sleep mode disable
+  epd_cmd(0x10);
+  epd_data(0x00);
+
+  // Set data entry mode (address x auto incremented and y will incrmeent)
+  epd_cmd(0x11);
+  epd_data(0x03);
+
+  // Set x start and end address
+  epd_cmd(0x44);
+  epd_data(0x00);
+  epd_data(0x11);
+
+  // Set y start and end address
+  epd_cmd(0x45);
+  epd_data(0x00);
+  epd_data(0xAB);
+
+  // Set inital x and y counter (move between start and end addresses)
+  epd_cmd(0x4E);
+  epd_data(0x00);
+  epd_cmd(0x4F);
+  epd_data(0x00);
+
+  // Set booster feedback to internal
+  epd_cmd(0xF0);
+  epd_data(0x1F);
+
+  // Write to VCOM register
+  epd_cmd(0x2C);
+  epd_data(0xA0);
+
+  epd_cmd(0x3C);
+  epd_data(0x63);
 }
--- port/gcc/m0+/include/drivers/port_epd.h
+++ port/gcc/m0+/include/drivers/port_epd.h
@@ -2,5 +2,8 @@
 #define __MRTOS_PORT_EPD__
 void epd_init();
 void epd_pwr_en();
+void epd_reset();
+uint8_t epd_busy();
+void epd_clr();
 
 #endif
