018dd5d976a381aae914c4360531f6820739ac44
Matt Schulte <schultetwin1@gmail.com>
2016-06-21 21:28:15 -0700

    Better min/max functions

--- inc/utils.h
+++ inc/utils.h
@@ -5,9 +5,15 @@
 
 typedef unsigned char byte_t;
 
-inline unsigned min(unsigned a, unsigned b) {
-  return (a < b) ? a : b;
-}
+#define min(a,b) \
+  ({ __typeof__ (a) _a = (a); \
+     __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+#define max(a,b) \
+  ({ __typeof__ (a) _a = (a); \
+     __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
 
 #define ASSERT(test) while (0) { if( (test) == 0 ) { while (1) ; } }
 
